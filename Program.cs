3a4b7c3cd466e970d4a4b5329ec499f5fa3d541a
paulmah <60945152+paulmah@users.noreply.github.com>
2020-05-09 19:17:25 -0600

    option 2 - file exist, need to read file and replace

--- Program.cs
+++ Program.cs
@@ -11,17 +11,20 @@ namespace c_
                         /*While/Do While Loop*/
 
             int endProg = 0; 
-
+            string filename = "";
             while (endProg != 3){ //A while loop will check the condition before executing the code
-                Console.WriteLine("Option 1: Enter the file you want to parse"); //Option  not necessary, but helps with user understanding
-                Console.WriteLine("Option 2: Enter a string you would like to replace with a blank space "); //Option  not necessary, but helps with user understanding
-                Console.WriteLine("Option 3: End Program"); //Option  not necessary, but helps with user understanding
+                Console.WriteLine("     Option 1: Enter the file you want to parse"); //Option  not necessary, but helps with user understanding
+                Console.WriteLine("     Option 2: Enter a string you would like to replace with a blank space "); //Option  not necessary, but helps with user understanding
+                Console.WriteLine("     Option 3: End Program"); //Option  not necessary, but helps with user understanding
+                Console.WriteLine("You selected file: " + filename ); // traceon
+
                 string option = Console.ReadLine();
+
                 Console.WriteLine("selected: " + option); // traceon
                 switch(option){
                     case "1": 
                         Console.WriteLine("Please enter the filename of the file you would like to parse");
-                        string filename = Console.ReadLine();
+                        filename = Console.ReadLine();
                         if ( File.Exists( filename ) ) { // does filename exist 
                             Console.WriteLine("You selected file: " + filename );
                         } else {
@@ -30,8 +33,27 @@ namespace c_
                         }
                         break;
                     case "2":
-                        Console.WriteLine("string you want to replace is ...");
+                        Console.WriteLine("You selected file: " + filename );
+                        if ( File.Exists( filename ) ) { // does filename exist 
+                            Console.WriteLine("Please enter the string you would like to replace with a space");
+                            string replacement = Console.ReadLine();
+                            Console.WriteLine("You replacement string is: " + replacement );
+
+                        } else {
+                            Console.WriteLine("Please choose 1 first and enter a valid filename first" );
+                        }
                         break;
+
+//             File.WriteAllText("nextexample.txt", "Hello Again, World!"); //creates file if it exists and adds content to it, overwrites file content if file exists
+//             readText = File.ReadAllText("nextexample.txt");
+//             Console.WriteLine(readText);
+//             File.Delete("nextexample.txt"); //Deletes a file
+
+
+
+
+
+
                     case "3":
                         endProg = 3;    // arbitrarily choose 3 to match the option to end program
                         break;
